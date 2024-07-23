93133a79917efdc864e5460bcbff94e72a01daf7
221B0825 <eunseoyu0825@gmail.com>
2024-07-23 17:28:36 +0900

    feat: navbar change

---
+++ ResourceManagementProject/WebContent/components/loadHeader.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/WebContent/components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-container").innerHTML = data;
+    });
+});
