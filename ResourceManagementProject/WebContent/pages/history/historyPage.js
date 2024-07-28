50f36f0967260fea3180cc7f961a6ca8f6c987fa
SEong_gu <sunggu507@naver.com>
2024-07-28 11:31:41 +0900

    [feat] 초기화 구현

--- ResourceManagementProject/WebContent/pages/history/historyPage.js
+++ ResourceManagementProject/WebContent/pages/history/historyPage.js
@@ -1,4 +1,5 @@
 let jsonAll = [];
+
 // 페이지 로드되면 바로 데이터 불러오게 하기.
 window.onload = fetchData;
 
@@ -29,7 +30,7 @@ function createTable(data) {
     row.innerHTML = `
       <td>${index + 1}</td>
       <td>
-        <input type="checkbox" />
+        <input type="checkbox" class="delete-checkbox" />
       </td>      
       <td>${item.category}</td>
       <td>${item.barcode}</td>
@@ -51,7 +52,6 @@ function createTable(data) {
 
 // 날짜 버튼 선택 시 dateStart 값 변경
 document.addEventListener("DOMContentLoaded", function () {
-  // 날짜 버튼 선택 시 dateStart 값 변경
   const today = new Date();
   const btnToday = document.getElementById("btnradio1");
   const btnWeek = document.getElementById("btnradio2");
@@ -101,16 +101,59 @@ document.addEventListener("DOMContentLoaded", function () {
       const searchMatch = item[searchType].toLowerCase().includes(searchBar);
 
       return (
+        (selectInput === "전체" || item.category === selectInput) &&
         itemDate >= dateStartValue &&
         itemDate <= dateEndValue &&
-        searchMatch &&
-        item.rack.includes(selectInput)
+        searchMatch
       );
     });
 
     createTable(filteredData);
   });
 
-  // 초기 데이터 테이블 채우기
+  // 초기화 버튼 클릭 시 필터 초기화
+  document
+    .querySelector(".button.btn-secondary")
+    .addEventListener("click", () => {
+      // 구분을 전체로 설정
+      document.getElementById("selectInput").value = "전체";
+
+      // 선택박스를 자재명으로 설정
+      document.getElementById("item").checked = true;
+
+      // 기간을 오늘로 설정
+      const today = new Date();
+      document.getElementById("dateStart").valueAsDate = today;
+      document.getElementById("dateEnd").valueAsDate = today;
+
+      // 검색창 초기화
+      document.getElementById("searchBar").value = "";
+
+      // 기간선택 버튼 초기화
+      document.getElementById("btnradio1").checked = true;
+
+      // 전체 내역으로 다시 출력
+      createTable(jsonAll);
+    });
+});
+
+// 삭제 버튼 클릭 시 선택된 항목 삭제
+document.querySelector(".btn-outline-danger").addEventListener("click", () => {
+  // 1. 모든 체크박스 요소를 선택하여 NodeList로 반환합니다.
+  const checkboxes = document.querySelectorAll(".delete-checkbox");
+
+  // 2. 각 체크박스를 반복하여 처리합니다.
+  checkboxes.forEach((checkbox) => {
+    // 3. 체크박스가 선택된 경우에만 처리합니다.
+    if (checkbox.checked) {
+      // 4. 체크박스의 data-index 속성을 가져와 정수로 변환합니다. 이 값은 배열에서 항목의 인덱스를 나타냅니다.
+      const index = parseInt(checkbox.dataset.index, 10);
+
+      // 5. 인덱스에 해당하는 항목을 jsonAll 배열에서 삭제합니다. splice 메서드는 배열에서 특정 항목을 제거합니다.
+      jsonAll.splice(index, 1);
+    }
+  });
+
+  // 6. jsonAll 배열의 현재 상태를 기준으로 테이블을 다시 만듭니다.
   createTable(jsonAll);
 });
